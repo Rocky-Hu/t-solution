064f308721ae05c3756bc99b8ec1961c4fa9f218
rocky <874904507@qq.com>
2022-08-04 17:44:50 +0800

    None

--- t-delay-message/src/main/java/org/solution/delaymessage/core/redis/RedisDelayMessageProducer.java
+++ t-delay-message/src/main/java/org/solution/delaymessage/core/redis/RedisDelayMessageProducer.java
@@ -16,6 +16,8 @@ public class RedisDelayMessageProducer implements DelayMessageProducer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisDelayMessageProducer.class);
 
+    private final ConcurrentHashMap<String, Object> parallelLockMap = new ConcurrentHashMap<>();
+
     private RedissonClient redissonClient;
     private ConcurrentHashMap<String, RDelayedQueue> delayedQueueRegistry = new ConcurrentHashMap<>();
     private Codec codec = new MarshallingCodec();
@@ -31,24 +33,45 @@ public class RedisDelayMessageProducer implements DelayMessageProducer {
 
     @Override
     public void send(DelayMessage delayMessage) {
+        LOGGER.debug("{}", delayMessage);
         delayedQueue(delayMessage.getTopic()).offer(delayMessage, delayMessage.getDelay(), delayMessage.getTimeUnit());
     }
 
     @Override
     public void sendAsync(DelayMessage delayMessage) {
+        LOGGER.debug("{}", delayMessage);
         delayedQueue(delayMessage.getTopic()).offerAsync(delayMessage, delayMessage.getDelay(), delayMessage.getTimeUnit());
     }
 
-    private RDelayedQueue<DelayMessage> delayedQueue(String topic) {
-        RDelayedQueue<DelayMessage> delayedQueue = delayedQueueRegistry.get(topic);
+    private RDelayedQueue<DelayMessage> delayedQueue(String name) {
+        RDelayedQueue<DelayMessage> delayedQueue = delayedQueueRegistry.get(name);
         if (delayedQueue != null) {
             return delayedQueue;
         } else {
-            RBlockingQueue<DelayMessage> blockingQueue = redissonClient.getBlockingQueue(topic);
-            delayedQueue = redissonClient.getDelayedQueue(blockingQueue);
-            delayedQueueRegistry.putIfAbsent(topic, delayedQueue);
-            return delayedQueue;
+            synchronized (getDelayedQueueInitialLock(name)) {
+                delayedQueue = delayedQueueRegistry.get(name);
+                if (delayedQueue == null) {
+                    RBlockingQueue<DelayMessage> initialBlockingQueue = redissonClient.getBlockingQueue(name);
+                    delayedQueue = redissonClient.getDelayedQueue(initialBlockingQueue);
+                    delayedQueueRegistry.put(name, delayedQueue);
+                }
+
+                return delayedQueue;
+            }
         }
+    }
+
+    private Object getDelayedQueueInitialLock(String name) {
+        Object lock = this;
+        if (parallelLockMap != null) {
+            Object newLock = new Object();
+            lock = parallelLockMap.putIfAbsent(name, newLock);
+            if (lock == null) {
+                lock = newLock;
+            }
+        }
+
+        return lock;
     }
 
 }
---
+++ t-delay-message/src/test/java/org/solution/delaymessage/RedisDelayMessageProducerTest.java
@@ -0,0 +1,15 @@
+package org.solution.delaymessage;
+
+import org.junit.jupiter.api.Test;
+
+public class RedisDelayMessageProducerTest {
+
+    @Test
+    public void test_send() {
+    }
+
+    @Test
+    public void test_sendAsync() {
+    }
+
+}
